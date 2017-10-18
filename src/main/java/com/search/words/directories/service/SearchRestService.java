5d87de1ad57487c0d8dc40062cfe8a6bb94237ca
Nandu Yenagandula <nandu.yenagandula@hibu.com>
2017-10-18 19:00:49 +0530

    Removed Unused imports

--- src/main/java/com/search/words/directories/service/SearchRestService.java
+++ src/main/java/com/search/words/directories/service/SearchRestService.java
@@ -10,15 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.search.words.directories.error.ErrorResponse;
 import com.search.words.directories.interfaces.SearchDirectories;
 import com.search.words.directories.request.dto.SearchRequest;
 import com.search.words.directories.response.dto.SearchResponse;
