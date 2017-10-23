b1bbaeda8668819b59e0f3eb858047c8efd32a2c
Nandu Yenagandula <nandu.yenagandula@hibu.com>
2017-10-23 16:41:43 +0530

    Made parameters Global

--- src/main/java/com/search/words/directories/async/config/AsyncCustomizeExecutor.java
+++ src/main/java/com/search/words/directories/async/config/AsyncCustomizeExecutor.java
@@ -4,9 +4,11 @@ import java.util.concurrent.Executor;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -15,15 +17,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 @PropertySource("classpath:application.properties")
 public class AsyncCustomizeExecutor extends AsyncConfigurerSupport{
+	
+	@Autowired
+	private Environment env;
 
 	@Override
 	@Bean(name = "threadPoolTaskExecutor")
 	public Executor getAsyncExecutor() {
 		ThreadPoolTaskExecutor taskExecutor=new ThreadPoolTaskExecutor();
-		taskExecutor.setCorePoolSize(10);
-		taskExecutor.setMaxPoolSize(10);
-		taskExecutor.setQueueCapacity(20);
-		taskExecutor.setThreadNamePrefix("Async-");
+		taskExecutor.setCorePoolSize(Integer.parseInt(env.getProperty("thread.core.pool.size")));
+		taskExecutor.setMaxPoolSize(Integer.parseInt(env.getProperty("thread.max.pool.size")));
+		taskExecutor.setQueueCapacity(Integer.parseInt(env.getProperty("thread.queue.capacity")));
+		taskExecutor.setThreadNamePrefix(env.getProperty("thread.name.prefix"));
 		taskExecutor.initialize();
 		return taskExecutor;
 	}
